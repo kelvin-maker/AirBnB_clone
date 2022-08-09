c59e990eac567050dc811e5e19468acd9d2c408c
SamuelRuby <youreajockey@gmail.com>
2022-08-09 18:49:08 +0200

    12 fix

--- console.py
+++ console.py
@@ -268,23 +268,6 @@ class HBNBCommand(cmd.Cmd):
         print("Prints all string representation of all instances")
         print("based or not on the class name")
         print("[Usage]: all <className>\n")
-    
-    def parse(arg):
-        curly_braces = re.search(r"\{(.*?)\}", arg)
-        brackets = re.search(r"\[(.*?)\]", arg)
-        if curly_braces is None:
-            if brackets is None:
-                return [i.strip(",") for i in split(arg)]
-            else:
-                lexer = split(arg[:brackets.span()[0]])
-                retl = [i.strip(",") for i in lexer]
-                retl.append(brackets.group())
-                return retl
-        else:
-            lexer = split(arg[:curly_braces.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(curly_braces.group())
-            return retl
 
     def do_count(self, arg):
         """Usage: count <class> or <class>.count()
